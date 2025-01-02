b2ff941047ebf74717c01b10fe058cfbdd191563
Mauricio Flores <itolisto@gmail.com>
2025-01-02 16:05:58 -0600

    chore: add locust test

---
+++ project2/Locust/locustfile.py
@@ -0,0 +1,22 @@
+import time, json, random
+from locust import HttpUser, task
+
+class PostGradesTest(HttpUser):
+
+  @task
+  def post_grades(self):
+    grades = []
+    with open("grades.json", "r") as file:
+      grades = json.load(file)
+    
+    for _ in range(len(grades)):
+      response = self.client.post("/login", json=grades.pop(), name="/gradePost")
+      print(response.text)
+      print(response.status_code)
+      time.sleep(random.choice([1,2]))
+
+  def on_start(self):
+    print("task started")
+
+  def on_stop(self):
+    print("task end")
