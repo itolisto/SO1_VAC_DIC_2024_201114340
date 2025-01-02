c82bee49861a112771a6b91cb3e68cca36b0e345
Mauricio Flores <itolisto@gmail.com>
2025-01-02 16:05:59 -0600

    bugfix: kafka producer

--- project2/gRPC/server/server.go
+++ project2/gRPC/server/server.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net"
 	"os"
+	"sync"
+	"time"
 
 	"google.golang.org/grpc"
 
@@ -15,11 +17,11 @@ import (
 )
 
 var (
-	topic = "assignment"
-
-	config          = sarama.NewConfig()
-	producer        sarama.AsyncProducer
-	kafkaBrokerUrl = fmt.Sprintf("%s:%s", os.Getenv("KAFKA_SERVER_HOST"), os.Getenv("KAFKA_SERVER_PORT"))
+	transactionIdGenerator int32 = 1
+	topic                        = "assignment"
+	producersLock          sync.Mutex
+	config                 = sarama.NewConfig()
+	kafkaBrokerUrl         = fmt.Sprintf("%s:%s", os.Getenv("KAFKA_SERVER_HOST"), os.Getenv("KAFKA_SERVER_PORT"))
 )
 
 type courseServer struct {
@@ -27,94 +29,39 @@ type courseServer struct {
 }
 
 func (s *courseServer) PostCourse(_ context.Context, grade *pb.CourseRecord) (*pb.CourseResponse, error) {
+	producersLock.Lock()
+	defer producersLock.Unlock()
 
-	err := producer.BeginTxn()
+	suffix := transactionIdGenerator
+	transactionIdGenerator++
+	fmt.Println(suffix)
+
+	config.Producer.Transaction.ID = config.Producer.Transaction.ID + "-" + fmt.Sprint(suffix)
+
+	localProducer, err := sarama.NewAsyncProducer([]string{kafkaBrokerUrl}, config)
+
+	if err != nil {
+		msg := fmt.Sprintf("gRPC server kafka new producer error: %v", err)
+		log.Println(msg)
+		return nil, err
+
+	}
+
+	err = localProducer.BeginTxn()
 
 	if err != nil {
 		msg := fmt.Sprintf("gRPC server kafka transaction producer error: %v", err)
-		log.Print(msg)
-		return &pb.CourseResponse{Response: msg}, nil
+		log.Println(msg)
+		return nil, err
 	}
 
-	producer.Input() <- &sarama.ProducerMessage{Topic: topic, Key: nil, Value: sarama.StringEncoder("test")}
-	err = producer.CommitTxn()
+	time.Sleep(50 * time.Microsecond)
+	localProducer.Input() <- &sarama.ProducerMessage{Topic: topic, Key: nil, Value: sarama.StringEncoder(grade.Curso)}
 
-	if err != nil {
-		log.Printf("Producer unable to commit transacction %s", err)
-
-		for {
-			if producer.TxnStatus()&sarama.ProducerTxnFlagFatalError != 0 {
-				// fatal error. need to recreate producer.
-				log.Printf("Producer: producer is in a fatal state, need to recreate it")
-				return &pb.CourseResponse{Response: "Kafka Producer: producer is in a fatal state, need to recreate it"}, nil
-			}
-
-			// If producer is in abortable state, try to abort current transaction.
-			if producer.TxnStatus()&sarama.ProducerTxnFlagAbortableError != 0 {
-				err = producer.AbortTxn()
-				if err != nil {
-					// If an error occured just retry it.
-					log.Printf("Producer: unable to abort transaction: %+v", err)
-					continue
-				}
-
-				return &pb.CourseResponse{Response: "Producer, unable to commit transaction"}, nil
-			}
-
-			// if not you can retry
-			err = producer.CommitTxn()
-
-			if err != nil {
-				log.Printf("Producer: unable to commit txn %s", err)
-				continue
-			}
-		}
-	}
+	localProducer.Close()
 
 	return &pb.CourseResponse{Response: "Assigment pushed to kafka Broker"}, nil
 }
-
-//     var (
-//         wg                                  sync.WaitGroup
-//         enqueued, successes, producerErrors int
-//     )
-
-//     wg.Add(1)
-//     go func() {
-//         defer wg.Done()
-//         for range producer.Successes() {
-//                 successes++
-//         }
-//     }()
-
-//     wg.Add(1)
-//     go func() {
-//         defer wg.Done()
-//         for err := range producer.Errors() {
-//                 log.Println(err)
-//                 producerErrors++
-//         }
-//     }()
-
-//     ProducerLoop:
-//     for {
-//         message := &ProducerMessage{Topic: "my_topic", Value: StringEncoder("testing 123")}
-//         select {
-//         case producer.Input() <- message:
-//             enqueued++
-
-//         case <-signals:
-//             producer.AsyncClose() // Trigger a shutdown of the producer.
-//             break ProducerLoop
-//         }
-//     }
-
-//     wg.Wait()
-
-//     log.Printf("Successfully produced: %d; errors: %d\n", successes, producerErrors)
-
-//     return &pb.CourseResponse{ Response: "gRPC server received course"}, nil
-// }
 
 func main() {
 	version, err := sarama.ParseKafkaVersion(sarama.MaxVersion.String())
@@ -131,19 +78,7 @@ func main() {
 	config.Producer.Transaction.Retry.Backoff = 10
 	config.Producer.Transaction.ID = "txn_producer"
 	config.Net.MaxOpenRequests = 1
-	config.Producer.Return.Successes = true
-
-	localProducer, err := sarama.NewAsyncProducer([]string{kafkaBrokerUrl}, config)
-
-	if err != nil {
-		msg := fmt.Sprintf("gRPC server kafka transaction producer error: %v", err)
-		log.Println(msg)
-		return
-
-	}
-
-	producer = localProducer
-	defer producer.AsyncClose()
+	// config.Producer.Return.Successes = true
 
 	url := fmt.Sprintf("%s:%s", os.Getenv("GRPC_SERVER_HOST"), os.Getenv("GRPC_SERVER_PORT"))
 	listener, err := net.Listen("tcp", url)
